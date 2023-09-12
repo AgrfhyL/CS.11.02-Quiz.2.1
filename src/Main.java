272b2cdf1a71b880d6cf2edecbbc5d0b854afb1e
agrfhyl <andyl2026@student.cis.edu.hk>
2023-09-12 14:08:29 +0800

    HII

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(1,1);
+        System.out.println(resultOne);
+        String resultTwo = greeting("Andy");
+        System.out.println(resultTwo);
+        int resultThree = add(1,1,1,1);
+        System.out.println(resultThree);
+        String toBePrinted = printMe("Hi");
+        System.out.println(toBePrinted);
+    }
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+    public static String greeting(String name){
+        return "\"" + "Bonjour, " + name + "!" + "\"";
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        return add(numOne, add(numTwo, add(numThree, numFour)));
+    }
+    public static String printMe(String toBePrinted){
+        return toBePrinted;
+    }
 }
